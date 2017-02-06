251171d78cbff5534495e6e1d61086510a3e9587
zhaowh3613 <zhaowh3613@outlook.com>
2017-02-07 01:04:22 +0800

    article page

--- django_blog/views.py
+++ django_blog/views.py
@@ -6,3 +6,7 @@ def index(request):
     # article = models.Article.objects.get(pk=1)
     articleList = models.Article.objects.all()
     return render(request, 'django_blog/index.html', {'articles': articleList})
+
+def article_page(request, article_id):
+    article = models.Article.objects.get(pk=article_id)
+    return render(request, 'django_blog/article_page.html', {'article': article})
