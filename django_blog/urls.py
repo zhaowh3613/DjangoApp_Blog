4deee8d49b3f3403a4d7a90fb5db023480e4d849
zhaowh <zhaowh3613@outlook.com>
2017-02-07 17:28:56 +0800

    update text

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^index/$', views.index),
+    url(r'^$', views.index, name='index'),
+    url(r'^index/$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
     url(r'^article/(?P<article_id>[0-9]+)$', views.article_page, name='article_page'),
 ]
