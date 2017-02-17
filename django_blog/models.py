ca50bc7acb6c1895015ee0fccaa351c015e24d02
zhaowh <zhaowh3613@outlook.com>
2017-02-17 16:38:32 +0800

    add post datetime

--- django_blog/models.py
+++ django_blog/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 class Article(models.Model):
     title = models.CharField(max_length=32, default='Title')
     content = models.TextField(null=True)
-    # createDate = models.DateTimeField(auto_now=True)
+    author = models.CharField(max_length=50, default='')
+    create_date = models.DateTimeField(auto_now=True)
+    update_date = models.DateTimeField(auto_now=False, default=datetime.now())
 
     def __str__(self):
     # def __unicode__(self):
